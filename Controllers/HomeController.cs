150aa2341f8b9f4694d16d0d3a0cfb80b18967d5
Carol Duarte <carolduarte2000@gmail.com>
2025-07-14 08:15:55 -0300

    Reorganiza estrutura do projeto e limpa arquivos obsoletos

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,38 @@
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using CasaMateriaisConstrucao.Models;
 
-namespace CasaMateriaisDeConstrucao.Controllers
+namespace CasaMateriaisConstrucao.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    [Route("")]
+    public IActionResult Index()
     {
-        [Route("")]
-        public IActionResult Index()
-        {
-            return View();
-        }
+        return View();
+    }
 
-        [Route("nossa-historia")]
-        public IActionResult NossaHistoria()
-        {
-            return View();
-        }
+    [Route("nossa-historia")]
+    public IActionResult NossaHistoria()
+    {
+        return View();
+    }
 
-        [Route("carreiras")]
-        public IActionResult Carreiras()
-        {
-            return View();
-        }
+    [Route("carreiras")]
+    public IActionResult Carreiras()
+    {
+        return View();
+    }
 
-        [Route("contato")]
-        public IActionResult Contato()
-        {
-            return View();
-        }
+    [Route("contato")]
+    public IActionResult Contato()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
